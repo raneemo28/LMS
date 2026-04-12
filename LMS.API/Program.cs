5a5250c52fba4948c4764c0360887da38f84150b
Raneemo28 <raneem2832004@gmail.com>
2026-04-12 12:45:52 +0300

    fix bugges

--- LMS.API/Program.cs
+++ LMS.API/Program.cs
@@ -6,6 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddInfrastructure(builder.Configuration);
+builder.Services.AddApplication();
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
@@ -18,9 +19,7 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 }
 
-app.UseHttpsRedirection();
-builder.Services.AddInfrastructure(builder.Configuration);
-builder.Services.AddApplication(); 
+app.UseHttpsRedirection(); 
 app.UseAuthorization();
 
 app.MapControllers();
