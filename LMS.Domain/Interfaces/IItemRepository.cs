59761dc002dbc4c45610a9e2080874cef319b453
Raneemo28 <raneem2832004@gmail.com>
2026-04-13 21:01:43 +0300

    add command to filter items

--- LMS.Domain/Interfaces/IItemRepository.cs
+++ LMS.Domain/Interfaces/IItemRepository.cs
@@ -1,9 +1,11 @@
 using LMS.Domain.Entities;
+using System.Linq.Expressions;
 
 namespace LMS.Domain.Interfaces
 {
     public interface IItemRepository : IResourceRepository
     {
         Task<Item?> GetItemWithFullDataAsync(int id);
+        Task<IEnumerable<Item>> GetItemsWithFullDataWithConditionAsync(Expression<Func<Item, bool>> filter);
     }
 }
--- LMS.infra/Repository/ItemRepository.cs
+++ LMS.infra/Repository/ItemRepository.cs
@@ -2,6 +2,7 @@ using LMS.Domain.Entities;
 using LMS.Domain.Interfaces;
 using LMS.infra.Database;
 using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
 
 namespace LMS.infra.Repository
 {
@@ -9,6 +10,18 @@ namespace LMS.infra.Repository
     {
         public ItemRepository(LibraryDbContext context) : base(context)
         {
+        }
+
+        public async Task<IEnumerable<Item>> GetItemsWithFullDataWithConditionAsync(Expression<Func<Item, bool>> filter)
+        {
+            return await _context.Items
+            .Include(i => i.Template)
+            .Include(i => i.Values)
+            .ThenInclude(p => p.Property)
+            .ThenInclude(v => v.Vocabulary)
+            .AsNoTracking()
+            .Where(filter) 
+            .ToListAsync();
         }
 
         public async Task<Item?> GetItemWithFullDataAsync(int id)
