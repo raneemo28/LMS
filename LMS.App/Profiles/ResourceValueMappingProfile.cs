68f13e396629afdc6ed17a7853c2fb716fef31fc
Ali Mrhj <mrhj26ali@gmail.com>
2026-06-22 19:43:54 +0300

    fix item update

--- LMS.App/Profiles/ResourceValueMappingProfile.cs
+++ LMS.App/Profiles/ResourceValueMappingProfile.cs
@@ -29,5 +29,17 @@ public class ResourceValueMappingProfile : Profile
             .ForMember(d => d.Resource,        opt => opt.Ignore())
             .ForMember(d => d.Property,        opt => opt.Ignore())
             .ForMember(d => d.ValueResource,   opt => opt.Ignore());
+
+        // =====================================================================
+        // THE FIX: Map ResourceValueDto to Value
+        // This is required because UpdateItemDto contains List<ResourceValueDto>
+        // and AutoMapper needs to know how to map it to List<Value>.
+        // =====================================================================
+        CreateMap<ResourceValueDto, Value>()
+            .ForMember(d => d.ResourceId,    opt => opt.Ignore()) // Will be set by AfterMap in ItemMappingProfile
+            .ForMember(d => d.ValueUri,      opt => opt.Ignore()) // System-derived
+            .ForMember(d => d.Resource,      opt => opt.Ignore()) // Navigation property
+            .ForMember(d => d.Property,      opt => opt.Ignore()) // Navigation property
+            .ForMember(d => d.ValueResource, opt => opt.Ignore()); // Navigation property
     }
 }
